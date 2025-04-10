bee8c96225280ef96e7823352b41519f43670eec
Sarthak Gupta <sarthakgupta@Sarthaks-MacBook-Pro.local>
2025-04-10 14:38:49 +0530

    check 2

---
+++ octofit-tracker/backend/octofit_tracker/test_data.py
@@ -0,0 +1,36 @@
+# Test data for the Octofit app
+
+test_data = {
+    "users": [
+        {"username": "thundergod", "email": "thundergod@mhigh.edu", "password": "thundergodpassword"},
+        {"username": "metalgeek", "email": "metalgeek@mhigh.edu", "password": "metalgeekpassword"},
+        {"username": "zerocool", "email": "zerocool@mhigh.edu", "password": "zerocoolpassword"},
+        {"username": "crashoverride", "email": "crashoverride@mhigh.edu", "password": "crashoverridepassword"},
+        {"username": "sleeptoken", "email": "sleeptoken@mhigh.edu", "password": "sleeptokenpassword"},
+    ],
+    "teams": [
+        {"name": "Blue Team"},
+        {"name": "Gold Team"},
+    ],
+    "activities": [
+        {"username": "thundergod", "activity_type": "Cycling", "duration": 60},
+        {"username": "metalgeek", "activity_type": "Crossfit", "duration": 120},
+        {"username": "zerocool", "activity_type": "Running", "duration": 90},
+        {"username": "crashoverride", "activity_type": "Strength", "duration": 30},
+        {"username": "sleeptoken", "activity_type": "Swimming", "duration": 75},
+    ],
+    "leaderboard": [
+        {"username": "thundergod", "score": 100},
+        {"username": "metalgeek", "score": 90},
+        {"username": "zerocool", "score": 95},
+        {"username": "crashoverride", "score": 85},
+        {"username": "sleeptoken", "score": 80},
+    ],
+    "workouts": [
+        {"name": "Cycling Training", "description": "Training for a road cycling event"},
+        {"name": "Crossfit", "description": "Training for a crossfit competition"},
+        {"name": "Running Training", "description": "Training for a marathon"},
+        {"name": "Strength Training", "description": "Training for strength"},
+        {"name": "Swimming Training", "description": "Training for a swimming competition"},
+    ],
+}
