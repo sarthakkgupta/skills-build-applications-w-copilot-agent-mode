10273d17e808d698432c0b0abe873c434c8666ad
Sarthak Gupta <sarthakgupta@Sarthaks-MacBook-Pro.local>
2025-04-10 14:58:43 +0530

    3 iteration

--- octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 from octofit_tracker.models import User, Team, Activity, Leaderboard, Workout
+from octofit_tracker.test_data import test_data
 from bson import ObjectId
-from datetime import timedelta
 
 class Command(BaseCommand):
     help = 'Populate the database with test data for users, teams, activities, leaderboard, and workouts'
@@ -14,52 +14,30 @@ class Command(BaseCommand):
         Leaderboard.objects.all().delete()
         Workout.objects.all().delete()
 
-        # Create users
-        users = [
-            User(_id=ObjectId(), username='thundergod', email='thundergod@mhigh.edu', password='thundergodpassword'),
-            User(_id=ObjectId(), username='metalgeek', email='metalgeek@mhigh.edu', password='metalgeekpassword'),
-            User(_id=ObjectId(), username='zerocool', email='zerocool@mhigh.edu', password='zerocoolpassword'),
-            User(_id=ObjectId(), username='crashoverride', email='crashoverride@mhigh.edu', password='crashoverridepassword'),
-            User(_id=ObjectId(), username='sleeptoken', email='sleeptoken@mhigh.edu', password='sleeptokenpassword'),
-        ]
+        # Populate users
+        users = [User(**user) for user in test_data['users']]
         User.objects.bulk_create(users)
 
-        # Create teams
-        team1 = Team(_id=ObjectId(), name='Blue Team')
-        team2 = Team(_id=ObjectId(), name='Gold Team')
-        team1.save()
-        team2.save()
-        team1.members.add(*users[:3])
-        team2.members.add(*users[3:])
+        # Populate teams
+        teams = [Team(**team) for team in test_data['teams']]
+        Team.objects.bulk_create(teams)
 
-        # Create activities
-        activities = [
-            Activity(_id=ObjectId(), user=users[0], activity_type='Cycling', duration=60),
-            Activity(_id=ObjectId(), user=users[1], activity_type='Crossfit', duration=120),
-            Activity(_id=ObjectId(), user=users[2], activity_type='Running', duration=90),
-            Activity(_id=ObjectId(), user=users[3], activity_type='Strength', duration=30),
-            Activity(_id=ObjectId(), user=users[4], activity_type='Swimming', duration=75),
-        ]
+        # Populate activities
+        activities = []
+        for activity in test_data['activities']:
+            user = User.objects.get(username=activity.pop('username'))
+            activities.append(Activity(user=user, **activity))
         Activity.objects.bulk_create(activities)
 
-        # Create leaderboard entries
-        leaderboard_entries = [
-            Leaderboard(_id=ObjectId(), user=users[0], score=100),
-            Leaderboard(_id=ObjectId(), user=users[1], score=90),
-            Leaderboard(_id=ObjectId(), user=users[2], score=95),
-            Leaderboard(_id=ObjectId(), user=users[3], score=85),
-            Leaderboard(_id=ObjectId(), user=users[4], score=80),
-        ]
+        # Populate leaderboard
+        leaderboard_entries = []
+        for entry in test_data['leaderboard']:
+            user = User.objects.get(username=entry.pop('username'))
+            leaderboard_entries.append(Leaderboard(user=user, **entry))
         Leaderboard.objects.bulk_create(leaderboard_entries)
 
-        # Create workouts
-        workouts = [
-            Workout(_id=ObjectId(), name='Cycling Training', description='Training for a road cycling event'),
-            Workout(_id=ObjectId(), name='Crossfit', description='Training for a crossfit competition'),
-            Workout(_id=ObjectId(), name='Running Training', description='Training for a marathon'),
-            Workout(_id=ObjectId(), name='Strength Training', description='Training for strength'),
-            Workout(_id=ObjectId(), name='Swimming Training', description='Training for a swimming competition'),
-        ]
+        # Populate workouts
+        workouts = [Workout(**workout) for workout in test_data['workouts']]
         Workout.objects.bulk_create(workouts)
 
         self.stdout.write(self.style.SUCCESS('Successfully populated the database with test data.'))
--- octofit-tracker/backend/octofit_tracker/test_data.py
+++ octofit-tracker/backend/octofit_tracker/test_data.py
@@ -1,36 +1,37 @@
-# Test data for the Octofit app
+from datetime import timedelta
+from bson import ObjectId
 
 test_data = {
     "users": [
-        {"username": "thundergod", "email": "thundergod@mhigh.edu", "password": "thundergodpassword"},
-        {"username": "metalgeek", "email": "metalgeek@mhigh.edu", "password": "metalgeekpassword"},
-        {"username": "zerocool", "email": "zerocool@mhigh.edu", "password": "zerocoolpassword"},
-        {"username": "crashoverride", "email": "crashoverride@mhigh.edu", "password": "crashoverridepassword"},
-        {"username": "sleeptoken", "email": "sleeptoken@mhigh.edu", "password": "sleeptokenpassword"},
+        {"_id": ObjectId(), "username": "thundergod", "email": "thundergod@mhigh.edu", "password": "thundergodpassword"},
+        {"_id": ObjectId(), "username": "metalgeek", "email": "metalgeek@mhigh.edu", "password": "metalgeekpassword"},
+        {"_id": ObjectId(), "username": "zerocool", "email": "zerocool@mhigh.edu", "password": "zerocoolpassword"},
+        {"_id": ObjectId(), "username": "crashoverride", "email": "crashoverride@mhigh.edu", "password": "crashoverridepassword"},
+        {"_id": ObjectId(), "username": "sleeptoken", "email": "sleeptoken@mhigh.edu", "password": "sleeptokenpassword"},
     ],
     "teams": [
-        {"name": "Blue Team"},
-        {"name": "Gold Team"},
+        {"_id": ObjectId(), "name": "Blue Team"},
+        {"_id": ObjectId(), "name": "Gold Team"},
     ],
     "activities": [
-        {"username": "thundergod", "activity_type": "Cycling", "duration": 60},
-        {"username": "metalgeek", "activity_type": "Crossfit", "duration": 120},
-        {"username": "zerocool", "activity_type": "Running", "duration": 90},
-        {"username": "crashoverride", "activity_type": "Strength", "duration": 30},
-        {"username": "sleeptoken", "activity_type": "Swimming", "duration": 75},
+        {"_id": ObjectId(), "username": "thundergod", "activity_type": "Cycling", "duration": timedelta(hours=1)},
+        {"_id": ObjectId(), "username": "metalgeek", "activity_type": "Crossfit", "duration": timedelta(hours=2)},
+        {"_id": ObjectId(), "username": "zerocool", "activity_type": "Running", "duration": timedelta(hours=1, minutes=30)},
+        {"_id": ObjectId(), "username": "crashoverride", "activity_type": "Strength", "duration": timedelta(minutes=30)},
+        {"_id": ObjectId(), "username": "sleeptoken", "activity_type": "Swimming", "duration": timedelta(hours=1, minutes=15)},
     ],
     "leaderboard": [
-        {"username": "thundergod", "score": 100},
-        {"username": "metalgeek", "score": 90},
-        {"username": "zerocool", "score": 95},
-        {"username": "crashoverride", "score": 85},
-        {"username": "sleeptoken", "score": 80},
+        {"_id": ObjectId(), "username": "thundergod", "score": 100},
+        {"_id": ObjectId(), "username": "metalgeek", "score": 90},
+        {"_id": ObjectId(), "username": "zerocool", "score": 95},
+        {"_id": ObjectId(), "username": "crashoverride", "score": 85},
+        {"_id": ObjectId(), "username": "sleeptoken", "score": 80},
     ],
     "workouts": [
-        {"name": "Cycling Training", "description": "Training for a road cycling event"},
-        {"name": "Crossfit", "description": "Training for a crossfit competition"},
-        {"name": "Running Training", "description": "Training for a marathon"},
-        {"name": "Strength Training", "description": "Training for strength"},
-        {"name": "Swimming Training", "description": "Training for a swimming competition"},
+        {"_id": ObjectId(), "name": "Cycling Training", "description": "Training for a road cycling event"},
+        {"_id": ObjectId(), "name": "Crossfit", "description": "Training for a crossfit competition"},
+        {"_id": ObjectId(), "name": "Running Training", "description": "Training for a marathon"},
+        {"_id": ObjectId(), "name": "Strength Training", "description": "Training for strength"},
+        {"_id": ObjectId(), "name": "Swimming Training", "description": "Training for a swimming competition"},
     ],
 }
